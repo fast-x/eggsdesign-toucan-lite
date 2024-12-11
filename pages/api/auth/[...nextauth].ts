dec1330f04979fea0f12f409ea2dcabeccb5dfbf
iamjianyoudai <daijianyou@gmail.com>
2024-12-11 18:34:12 +0100

    update with PKCE config

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,10 +11,12 @@ export default NextAuth({
       tenantId: process.env.AZURE_AD_TENANT_ID,
       authorization: {
         params: {
-          scopes: process.env.AZURE_AD_SCOPES,
+          scope: process.env.AZURE_AD_SCOPES || 'openid profile email',
+          response_type: 'code',
+          code_challenge_method: 'S256',
         },
       },
-      checks: ['pkce', 'state'],
+      checks: ['pkce', 'state'], 
     }),
   ],
   callbacks: {
@@ -32,3 +34,37 @@ export default NextAuth({
   },
   debug: true,
 });
+
+// import NextAuth, { Session, User } from 'next-auth';
+// import { JWT } from 'next-auth/jwt';
+// import AzureADProvider from 'next-auth/providers/azure-ad';
+
+// export default NextAuth({
+//   secret: process.env.NEXTAUTH_SECRET,
+//   providers: [
+//     AzureADProvider({
+//       clientId: process.env.AZURE_AD_CLIENT_ID || '',
+//       clientSecret: process.env.AZURE_AD_CLIENT_SECRET || '',
+//       tenantId: process.env.AZURE_AD_TENANT_ID,
+//       authorization: {
+//         params: {
+//           scopes: process.env.AZURE_AD_SCOPES,
+//         },
+//       },
+//     }),
+//   ],
+//   callbacks: {
+//     async jwt({ token }: { token: JWT }) {
+//       return token;
+//     },
+//     async session({ session, user }: { session: Session; user: User }) {
+//       session.user = {
+//         name: user.name,
+//         email: user.email,
+//       };
+
+//       return session;
+//     },
+//   },
+//   debug: true,
+// });
