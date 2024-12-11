3b606eb2308dd0e31f5ec4c288e8520c07ba6db2
iamjianyoudai <daijianyou@gmail.com>
2024-12-11 17:28:17 +0100

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,9 +12,9 @@ export default NextAuth({
       authorization: {
         params: {
           scopes: process.env.AZURE_AD_SCOPES,
-          checks: ['pkce', 'state'],
         },
       },
+      checks: ['pkce', 'state'],
     }),
   ],
   callbacks: {
