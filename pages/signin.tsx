dec3498168310bf31a3d15c3c34b821bc2f08481
iamjianyoudai <daijianyou@gmail.com>
2024-12-11 14:44:10 +0100

    test5

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -21,7 +21,7 @@ export default function Signin() {
     if (status === 'unauthenticated') {
       console.log('No JWT');
 
-      console.log('2 - TEST - add error msg in signin')
+      console.log('2 - TEST - add error msg in signin');
       void signIn('azure-ad', { callbackUrl: '/' }).catch((err) => console.error('Sign-in error:', err));
     } else if (status === 'authenticated') {
       void router.push('/');
