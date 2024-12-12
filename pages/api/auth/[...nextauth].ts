ee1fe9dae405bfbf5516534bdd8819e28bc2afd4
iamjianyoudai <daijianyou@gmail.com>
2024-12-12 12:20:53 +0100

    rm  clientSecret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -58,7 +58,6 @@ export default NextAuth({
     AzureADProvider({
       clientId: process.env.AZURE_AD_CLIENT_ID || '',
       tenantId: process.env.AZURE_AD_TENANT_ID,
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET || '',
       authorization: {
         params: {
           response_type: 'code',
