6347ed8263bb515e6b5c59369ebceb464385d428
iamjianyoudai <daijianyou@gmail.com>
2024-12-12 12:04:45 +0100

    update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,24 +1,19 @@
-import NextAuth from 'next-auth';
-import AzureADProvider from 'next-auth/providers/azure-ad';
-
-export default NextAuth({
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    AzureADProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID,
-      tenantId: process.env.AZURE_AD_TENANT_ID,
-      authorization: {
-        params: {
-          scope: process.env.AZURE_AD_SCOPES,
-          response_type: 'code',
-          code_challenge_method: 'S256',
-        },
-      },
-      checks: ['pkce', 'state'],
-    } as any),
-  ],
-  debug: true,
-});
+// export default NextAuth({
+//   secret: process.env.NEXTAUTH_SECRET,
+//   providers: [
+//     AzureADProvider({
+//       clientId: '7374dd2c-04bd-47bc-b685-90db430810a1',
+//       tenantId: '8b87af7d-8647-4dc7-8df4-5f69a2011bb5',
+//       authorization: {
+//         params: {
+//           scope: 'api://7374dd2c-04bd-47bc-b685-90db430810a1/Read',
+//         },
+//       },
+//       checks: ['pkce', 'state'],
+//     } as any),
+//   ],
+//   debug: true,
+// });
 
 // export default NextAuth({
 //   secret: process.env.NEXTAUTH_SECRET,
@@ -53,36 +48,38 @@ export default NextAuth({
 //   debug: true,
 // });
 
-// import NextAuth, { Session, User } from 'next-auth';
-// import { JWT } from 'next-auth/jwt';
-// import AzureADProvider from 'next-auth/providers/azure-ad';
+import NextAuth, { Session, User } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import AzureADProvider from 'next-auth/providers/azure-ad';
 
-// export default NextAuth({
-//   secret: process.env.NEXTAUTH_SECRET,
-//   providers: [
-//     AzureADProvider({
-//       clientId: process.env.AZURE_AD_CLIENT_ID || '',
-//       clientSecret: process.env.AZURE_AD_CLIENT_SECRET || '',
-//       tenantId: process.env.AZURE_AD_TENANT_ID,
-//       authorization: {
-//         params: {
-//           scopes: process.env.AZURE_AD_SCOPES,
-//         },
-//       },
-//     }),
-//   ],
-//   callbacks: {
-//     async jwt({ token }: { token: JWT }) {
-//       return token;
-//     },
-//     async session({ session, user }: { session: Session; user: User }) {
-//       session.user = {
-//         name: user.name,
-//         email: user.email,
-//       };
-
-//       return session;
-//     },
-//   },
-//   debug: true,
-// });
+export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID || '',
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET || '',
+      authorization: {
+        params: {
+          response_type: 'code',
+          scope: process.env.AZURE_AD_SCOPES || 'openid profile email',
+          code_challenge_method: 'S256',
+        },
+      },
+      checks: ['pkce', 'state'],
+    } as any),
+  ],
+  callbacks: {
+    async jwt({ token }: { token: JWT }) {
+      return token;
+    },
+    async session({ session, user }: { session: Session; user: User }) {
+      session.user = {
+        name: user.name,
+        email: user.email,
+      };
+      return session;
+    },
+  },
+  debug: true,
+});
